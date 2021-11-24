c9c6b6c6e1ac3e2a08c43d9052b954a69351595b
Mint014 <89176620+Mint014@users.noreply.github.com>
2021-11-24 14:02:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -151,15 +151,19 @@ def event_handle(event):
         msg = str(event["message"]["text"])
         if msg == "สวัสดี":
             replyObj = TextSendMessage(text="จ้า ดีด้วยจ้า")
+            line_bot_api.reply_message(rtoken, replyObj)
         elif msg == "กินข้าวไหม":
             replyObj = TextSendMessage(text="ไม่ล่ะ")
+            line_bot_api.reply_message(rtoken, replyObj)
         elif msg == "ไปเที่ยวกันไหม":
             replyObj = TextSendMessage(text="ไปดิ")
+            line_bot_api.reply_message(rtoken, replyObj)
         elif msg == "โอเค":
             replyObj = TextSendMessage(text="จ้าๆ")
+            line_bot_api.reply_message(rtoken, replyObj)
         else:
             replyObj = TextSendMessage(text=msg) 
-        line_bot_api.reply_message(rtoken, replyObj)
+            line_bot_api.reply_message(rtoken, replyObj)
     elif msgType == "image":
         try:
             message_content = line_bot_api.get_message_content(event['message']['id'])
